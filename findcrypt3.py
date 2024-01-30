c48dfb4395b9ee7e2a6078bef4b1dacfa4f9b622
walden <anzewei88@gmail.com>
2024-01-30 11:19:08 +0800

    Update findcrypt3.py

--- findcrypt3.py
+++ findcrypt3.py
@@ -186,25 +186,26 @@ class Findcrypt_Plugin_t(idaapi.plugin_t):
         values = list()
         matches = rules.match(data=memory)
         for match in matches:
-            for string in match.strings:
+            for stringR in match.strings:
                 name = match.rule
-                if name.endswith("_API"):
-                    try:
-                        name = name + "_" + idc.GetString(self.toVirtualAddress(string[0], offsets))
-                    except:
-                        pass
-                value = [
-                    self.toVirtualAddress(string[0], offsets),
-                    match.namespace,
-                    name + "_" + hex(self.toVirtualAddress(string[0], offsets)).lstrip("0x").rstrip("L").upper(),
-                    string[1],
-                    repr(string[2]),
-                ]
-                idaapi.set_name(value[0], name
+                for string in stringR.instances:
+                    if name.endswith("_API"):
+                        try:
+                            name = name + "_" + idc.GetString(self.toVirtualAddress(string.offset, offsets))
+                        except:
+                            pass
+                    value = [
+                        self.toVirtualAddress(string.offset, offsets),
+                        match.namespace,
+                        name + "_" + hex(self.toVirtualAddress(string.offset, offsets)).lstrip("0x").rstrip("L").upper(),
+                        stringR.identifier,
+                        repr(string.matched_data)
+                    ]
+                    idaapi.set_name(value[0], name
                              + "_"
-                             + hex(self.toVirtualAddress(string[0], offsets)).lstrip("0x").rstrip("L").upper()
+                             + hex(self.toVirtualAddress(string.offset, offsets)).lstrip("0x").rstrip("L").upper()
                              , 0)
-                values.append(value)
+                    values.append(value)
         print("<<< end yara search")
         return values
 
